feedb311564571d615fb2370788b4682f749e0b7
pattabhia <shivaram2004@gmail.com>
2020-04-18 13:51:02 +0530

    binding drools session

--- drools-parent/drools-example/src/main/java/com/abhiram/drools/example/controller/PurchaseController.java
+++ drools-parent/drools-example/src/main/java/com/abhiram/drools/example/controller/PurchaseController.java
@@ -1,6 +1,8 @@
 package com.abhiram.drools.example.controller;
 
 import com.abhiram.drools.example.model.Order;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PurchaseController {
 
+    @Autowired
+    private KieSession session;
+
     @PostMapping("/order")
-    public String createOrder(@RequestBody Order order) {
-        return null;
+    public Order createOrder(@RequestBody Order order) {
+        session.insert(order);
+        session.fireAllRules();
+        return order;
     }
 }
