8d15d6529fb067f62c6ecf496b37866ab70969f7
pattabhia <shivaram2004@gmail.com>
2020-04-18 12:26:03 +0530

    creating project
    creating project

---
+++ drools-parent/drools-example/src/main/java/com/abhiram/drools/example/DroolsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.drools.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsExampleApplication.class, args);
+    }
+
+}
---
+++ drools-parent/drools-example/src/main/java/com/abhiram/drools/example/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.abhiram.drools.example;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DroolsExampleApplication.class);
+    }
+
+}
